513e0522df7336b89c8f72b85cbe7d92fe0238ff
Gregor Iwanowski <s83250@beuth-hochschule.de>
2021-10-20 14:35:28 +0200

    BAS2 fertig

--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
@@ -1,12 +1,20 @@
 package org.dieschnittstelle.ess.basics;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dieschnittstelle.ess.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.ess.basics.annotations.StockItemProxyImpl;
+import org.dieschnittstelle.ess.basics.reflection.ReflectedStockItemBuilder;
+
+import java.lang.reflect.Field;
 
 import static org.dieschnittstelle.ess.utils.Utils.*;
 
 public class ShowAnnotations {
+
+	protected static Logger logger = LogManager
+			.getLogger(ShowAnnotations.class);
 
 	public static void main(String[] args) {
 		// we initialise the collection
@@ -37,6 +45,22 @@ public class ShowAnnotations {
 		//  will then be built from the field names and field values.
 		//  Note that only read-access to fields via getters or direct access
 		//  is required here.
+
+		Class klass = instance.getClass();
+		String representation = "{" + klass.getSimpleName();
+		try {
+			for (Field f : klass.getDeclaredFields()) {
+				f.setAccessible(true);
+				representation += " " + f.getName() + ":" + f.get(instance) + ",";
+			}
+
+			representation = representation.substring(0, representation.length() - 1) + "}";
+			logger.info(representation + "\n");
+
+		} catch (IllegalAccessException e) {
+			logger.error("got IllegalAccessException: " + e, e);
+			throw new RuntimeException(e);
+		}
 
 		// TODO BAS3: if the new @DisplayAs annotation is present on a field,
 		//  the string representation will not use the field's name, but the name
